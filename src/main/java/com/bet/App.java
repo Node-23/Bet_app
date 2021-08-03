0871e02417c8dc3177fd6ba72094116d941f3244
Node-23 <matheusvinicius38@live.com>
2021-08-03 18:41:50 -0300

    Add actionListeners to all JMenuItems in mainView and RegisterView created

--- src/main/java/com/bet/App.java
+++ src/main/java/com/bet/App.java
@@ -1,14 +1,11 @@
 package com.bet;
 
-import javax.swing.JFrame;
-
 import com.bet.View.MainView;
 
 public class App {
     
     public static void main(String[] args) {
-        
-        JFrame mainView = new MainView(); 
+        new MainView(); 
     }
 
 }
--- src/main/java/com/bet/View/MainView.java
+++ src/main/java/com/bet/View/MainView.java
@@ -1,6 +1,9 @@
 package com.bet.View;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -8,7 +11,23 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class MainView extends JFrame{
+public class MainView extends JFrame implements ActionListener{
+
+    JMenuItem registerItem = new JMenuItem("Register");
+    JMenuItem editItem = new JMenuItem("Edit");
+    JMenuItem listItem = new JMenuItem("List Bettors");
+    JMenuItem deleteItem = new JMenuItem("Delete");
+
+    JMenuItem newBetItem = new JMenuItem("New Bet");
+    JMenuItem editBetItem = new JMenuItem("Edit Bet");
+    JMenuItem listBetItem = new JMenuItem("List Bets");
+    JMenuItem endBetItem = new JMenuItem("End Bet");
+
+    JMenuItem drawItem = new JMenuItem("Draw Bet");
+    JMenuItem listDrawItem = new JMenuItem("List Draws");
+
+    JMenuItem howToUseItem = new JMenuItem("How can i use it?");
+    JMenuItem aboutItem = new JMenuItem("About");
     
     public MainView(){
         //Frame settings----------------------------------
@@ -20,51 +39,95 @@ public class MainView extends JFrame{
 
         //Menubars settings----------------------------------    
         JMenuBar menuBar = new JMenuBar();
-        
         JMenu bettorMenu = new JMenu("Bettors");
         JMenu betMenu = new JMenu("Bets");
         JMenu drawMenu = new JMenu("Draw");
         JMenu helpMenu = new JMenu("Help");
-
-        JMenuItem registerItem = new JMenuItem("Register");
-        JMenuItem editItem = new JMenuItem("Edit");
-        JMenuItem listItem = new JMenuItem("List Bettors");
-        JMenuItem deleteItem = new JMenuItem("Delete");
+        
         bettorMenu.add(registerItem);
         bettorMenu.add(editItem);
         bettorMenu.add(listItem);
         bettorMenu.add(deleteItem);
-
-        JMenuItem newBetItem = new JMenuItem("New Bet");
-        JMenuItem editBetItem = new JMenuItem("Edit Bet");
-        JMenuItem listBetItem = new JMenuItem("List Bets");
-        JMenuItem endBetItem = new JMenuItem("End Bet");
         betMenu.add(newBetItem);
         betMenu.add(editBetItem);
         betMenu.add(listBetItem);
         betMenu.add(endBetItem);
-
-        JMenuItem drawItem = new JMenuItem("Draw Bet");
-        JMenuItem listDrawItem = new JMenuItem("List Draws");
         drawMenu.add(drawItem);
         drawMenu.add(listDrawItem);
-
-        JMenuItem howToUseItem = new JMenuItem("How can i use it?");
         helpMenu.add(howToUseItem);
+        helpMenu.add(aboutItem);
 
+        //Menubar action Events---------------------------------------------
+        registerItem.addActionListener(this);
+        editItem.addActionListener(this);
+        listItem.addActionListener(this);
+        deleteItem.addActionListener(this);
+        newBetItem.addActionListener(this);
+        editBetItem.addActionListener(this);
+        listBetItem.addActionListener(this);
+        endBetItem.addActionListener(this);
+        drawItem.addActionListener(this);
+        listDrawItem.addActionListener(this);
+        howToUseItem.addActionListener(this);
+        aboutItem.addActionListener(this);
+
+        //Menubar adds------------------------------------------------------
         menuBar.add(bettorMenu);
         menuBar.add(betMenu);
         menuBar.add(drawMenu);
         menuBar.add(helpMenu);
         this.setJMenuBar(menuBar);
 
-        //Label settings---------------------------------- 
+        //Label settings----------------------------------------------------
         JLabel welcomeLabel = new JLabel("Welcome to Bet App!");
         welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
         welcomeLabel.setVerticalAlignment(JLabel.CENTER);
         welcomeLabel.setForeground(new Color(0x082032));
+        welcomeLabel.setFont(new Font("Modern No. 20",Font.PLAIN,20));
 
         this.add(welcomeLabel);
         this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == registerItem) {
+            this.dispose();
+            new RegisterView();
+        }
+        if (e.getSource() == editItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == listItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == deleteItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == newBetItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == editBetItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == listBetItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == endBetItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == drawItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == listDrawItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == howToUseItem) {
+            // TODO Auto-generated method stub
+        }
+        if (e.getSource() == aboutItem) {
+            // TODO Auto-generated method stub
+        }
+        
     }
 }
