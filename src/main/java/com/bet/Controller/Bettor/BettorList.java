c3935e8f19963f88e0c7d0eb965ec859c9418358
Matheus Vinicius <54945311+Node-23@users.noreply.github.com>
2021-08-15 16:54:49 -0300

    ListItemView now is connected with database (#42)
    Co-authored-by: Node-23 <matheus.dev@hotmail.com>

---
+++ src/main/java/com/bet/Controller/Bettor/BettorList.java
@@ -0,0 +1,40 @@
+package com.bet.Controller.Bettor;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BettorList {
+    final static int tableColumnQuantity = 6;
+
+    public static Object[][] getBettors(Connection connection) {
+        Object[][] bettorsList = null;
+        int tableLenght = 0;
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet r = statement.executeQuery("SELECT COUNT(*) AS rowcount FROM bettors");
+            r.next();
+            tableLenght = r.getInt("rowcount");
+            r.close();
+            ResultSet rs = statement.executeQuery("SELECT * FROM bettors");
+            bettorsList = new Object[tableLenght][tableColumnQuantity];
+            int rowCounter = 0;
+            while (rs.next()) {
+                bettorsList[rowCounter][0] = rs.getString("name");
+                bettorsList[rowCounter][1] = rs.getString("cpf");
+                bettorsList[rowCounter][2] = rs.getString("mainphonenumber");
+                bettorsList[rowCounter][3] = rs.getString("secondaryphonenumber");
+                bettorsList[rowCounter][4] = "Edit";
+                bettorsList[rowCounter][5] = "Delete";
+                rowCounter++;
+                System.out.println(rowCounter);
+            }
+            statement.close();
+        } catch (SQLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        return bettorsList;
+    }
+}
